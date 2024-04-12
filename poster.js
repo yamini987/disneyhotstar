a42b6d3c2cc94831ae4729dc3ff3a1d3facb47ac
yamini987 <134353643+yamini987@users.noreply.github.com>
2024-04-12 15:08:41 +0530

    Create poster.js

---
+++ poster.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'loki',
+        des: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam perspiciatis sit commodi, non error libero neque! Dolorem, harum repellendus officia modi ut deserunt cumque accusamus natus dicta nihil, sunt accusantium?',
+        image: 'slider1.png'
+    },
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam perspiciatis sit commodi, non error libero neque! Dolorem, harum repellendus officia modi ut deserunt cumque accusamus natus dicta nihil, sunt accusantium?',
+        image: 'slider2.png'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam perspiciatis sit commodi, non error libero neque! Dolorem, harum repellendus officia modi ut deserunt cumque accusamus natus dicta nihil, sunt accusantium?',
+        image: 'slider3.png'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam perspiciatis sit commodi, non error libero neque! Dolorem, harum repellendus officia modi ut deserunt cumque accusamus natus dicta nihil, sunt accusantium?',
+        image: 'slider4.png'
+    },
+    {
+        name: 'lota',
+        des: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam perspiciatis sit commodi, non error libero neque! Dolorem, harum repellendus officia modi ut deserunt cumque accusamus natus dicta nihil, sunt accusantium?',
+        image: 'slider5.png'
+    }
+]
